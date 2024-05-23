1c3b926352b0a75fc3da08150dda9f84417993f5
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2024-05-23 08:34:21 -0300

    base server

---
+++ server.js
@@ -0,0 +1,12 @@
+port = 3000;
+
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello world"})
+})
+
+app.listen(port); {
+    console.log('Server is running on port ' + port);
+}
