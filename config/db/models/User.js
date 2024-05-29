dcf352bc29fdc20597a52a527ae8f642383a9218
Murilo Lima Matioli <murilo.matioli@pmisa.net>
2024-05-29 08:52:31 -0300

    Rota de login criada

--- config/db/models/User.js
+++ config/db/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema ({
     username: String,
-    senha: String,
+    password: String,
 })
 
 module.exports = mongoose.model("users", userSchema)
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ app.use(express.json());
 
 require("./config/db/connection");
 const User = require('./config/db/models/User');
-
-
 const nossoToken = require('./jwt/autentication')
 
 app.get('/', (req, res) => {
@@ -15,16 +13,54 @@ app.get('/', (req, res) => {
 
 
 app.post('/users', async (req, res) => {
-    const { username, senha } = req.body;
+    const { username, password } = req.body;
     try {
-        const newUser = new User({ username, senha });
+        const newUser = new User({ username, password });
         const userSave = await newUser.save();
         res.status(201).json(userSave);
     } catch (err) {
         res.status(400).json({ message: err.message})
     }
 })
-
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users)
+    } catch (err){
+        res.status(401).json({ message: err.message});
+    }
+})
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username })
+        const findPassword = await User.findOne({ password })
+        if(!user) {
+            return res.status(402).json({ message: "Username incorreto"});
+        }
+        if(!findPassword) {
+            return res.status(403).json({ message: "Senha incorreta"})
+        }
+        if(user && findPassword){
+            return res.status(200).json({ message: "Autenticado como " + req.body.username});
+        }
+    } catch (err){
+        return res.status(500).json({ message: "Erro com o servidor"});
+    }
+})
+app.delete('/users', async (req, res) => {
+    try {
+        const deleteUser = await User.deleteMany({});
+        if(deleteUser.deletedCount == 0){
+            return res.json({ message: "Não há usuários para deletar"});
+        }else{
+            return res.json({ message: `${deleteUser.deletedCount} usuários deletados`})
+        }
+        
+    } catch{
+        return res.json({ message: "Erro"});
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
